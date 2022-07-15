dec33081ecf4574c6b8fc007da50f67b35f283bd
Dominic DiGiacomo <ddigiacomo@triptychlabs.io>
2022-07-15 13:42:26 -0700

    questing wasm size optimize

--- src/utils/wasm_loader_hooks.tsx
+++ src/utils/wasm_loader_hooks.tsx
@@ -1,121 +1,130 @@
 import {useEffect, useState} from "react";
+import pako from 'pako';
 
 export const useSomeplaceWasm = () => {
-  // Keep track of script status ("idle", "loading", "ready", "error")
-  const [status, setStatus] = useState("loading");
-  useEffect(
-    () => {
-      if (!WebAssembly.instantiateStreaming) {
-        // polyfill
-        // @ts-ignore
-        WebAssembly.instantiateStreaming = async (resp, importObject) => {
-          return await WebAssembly.instantiate(resp, importObject);
-        };
-      }
-      // @ts-ignore
-      const go = new Go();
-      // @ts-ignore
-      let mod, inst;
-      async function goInit() {
-        try {
-          const bytes = await fetch("/someplace.wasm");
-          const result = await WebAssembly.instantiateStreaming(
-            bytes,
-            go.importObject
-          );
-          mod = result.module;
-          inst = result.instance;
-          go.run(inst);
-          setStatus("ready");
-        } catch (err) {
-          console.error(err);
-          setStatus("error");
-        }
-      }
+    // Keep track of script status ("idle", "loading", "ready", "error")
+    const [status, setStatus] = useState("loading");
+    useEffect(
+        () => {
+            if (!WebAssembly.instantiateStreaming) {
+                // polyfill
+                // @ts-ignore
+                WebAssembly.instantiateStreaming = async (resp, importObject) => {
+                    return await WebAssembly.instantiate(resp, importObject);
+                };
+            }
+            // @ts-ignore
+            const go = new Go();
+            // @ts-ignore
+            let mod, inst;
+            async function goInit() {
+                try {
+                    const bytes = await fetch("/someplace.wasm");
+                    const result = await WebAssembly.instantiateStreaming(
+                        bytes,
+                        go.importObject
+                    );
+                    mod = result.module;
+                    inst = result.instance;
+                    go.run(inst);
+                    setStatus("ready");
+                } catch (err) {
+                    console.error(err);
+                    setStatus("error");
+                }
+            }
 
-      goInit();
-    },
-    [] // Only re-run effect if script src changes
-  );
-  return status;
+            goInit();
+        },
+        [] // Only re-run effect if script src changes
+    );
+    return status;
 };
 
 export const useQuestingWasm = () => {
-  // Keep track of script status ("idle", "loading", "ready", "error")
-  const [status, setStatus] = useState("loading");
-  useEffect(
-    () => {
-      if (!WebAssembly.instantiateStreaming) {
-        // polyfill
-        // @ts-ignore
-        WebAssembly.instantiateStreaming = async (resp, importObject) => {
-          return await WebAssembly.instantiate(resp, importObject);
-        };
-      }
-      // @ts-ignore
-      const go = new Go();
-      // @ts-ignore
-      let mod, inst;
-      async function goInit() {
-        try {
-          const bytes = await fetch("/questing.wasm");
-          const result = await WebAssembly.instantiateStreaming(
-            bytes,
-            go.importObject
-          );
-          mod = result.module;
-          inst = result.instance;
-          go.run(inst);
-          setStatus("ready");
-        } catch (err) {
-          console.error(err);
-          setStatus("error");
-        }
-      }
+    // Keep track of script status ("idle", "loading", "ready", "error")
+    const [status, setStatus] = useState("loading");
+    useEffect(
+        () => {
+            if (!WebAssembly.instantiateStreaming) {
+                // polyfill
+                // @ts-ignore
+                WebAssembly.instantiateStreaming = async (resp, importObject) => {
+                    return await WebAssembly.instantiate(resp, importObject);
+                };
+            }
+            // @ts-ignore
+            const go = new Go();
+            // @ts-ignore
+            let mod, inst;
+            async function goInit() {
+                try {
+                    // const bytes = await fetch("/questing.wasm");
+                    let bytes = pako.ungzip(await (await fetch("/questing.wasm.gz")).arrayBuffer());
+                    // A fetched response might be decompressed twice on Firefox.
+                    // See https://bugzilla.mozilla.org/show_bug.cgi?id=610679
+                    if (bytes[0] === 0x1f && bytes[1] === 0x8b) {
+                        bytes = pako.ungzip(bytes);
+                    }
 
-      goInit();
-    },
-    [] // Only re-run effect if script src changes
-  );
-  return status;
+                    const result = await WebAssembly.instantiate(
+                        //@ts-ignore
+                        bytes,
+                        go.importObject
+                    );
+                    mod = result.module;
+                    inst = result.instance;
+                    go.run(inst);
+                    setStatus("ready");
+                } catch (err) {
+                    console.error(err);
+                    setStatus("error");
+                }
+            }
+
+            goInit();
+        },
+        [] // Only re-run effect if script src changes
+    );
+    return status;
 };
 
 export const useSwapperWasm = () => {
-  // Keep track of script status ("idle", "loading", "ready", "error")
-  const [status, setStatus] = useState("loading");
-  useEffect(
-    () => {
-      if (!WebAssembly.instantiateStreaming) {
-        // polyfill
-        // @ts-ignore
-        WebAssembly.instantiateStreaming = async (resp, importObject) => {
-          return await WebAssembly.instantiate(resp, importObject);
-        };
-      }
-      // @ts-ignore
-      const go = new Go();
-      // @ts-ignore
-      let mod, inst;
-      async function goInit() {
-        try {
-          const bytes = await fetch("/swapper.wasm");
-          const result = await WebAssembly.instantiateStreaming(
-            bytes,
-            go.importObject
-          );
-          mod = result.module;
-          inst = result.instance;
-          go.run(inst);
-          setStatus("ready");
-        } catch (err) {
-          console.error(err);
-          setStatus("error");
-        }
-      }
+    // Keep track of script status ("idle", "loading", "ready", "error")
+    const [status, setStatus] = useState("loading");
+    useEffect(
+        () => {
+            if (!WebAssembly.instantiateStreaming) {
+                // polyfill
+                // @ts-ignore
+                WebAssembly.instantiateStreaming = async (resp, importObject) => {
+                    return await WebAssembly.instantiate(resp, importObject);
+                };
+            }
+            // @ts-ignore
+            const go = new Go();
+            // @ts-ignore
+            let mod, inst;
+            async function goInit() {
+                try {
+                    const bytes = await fetch("/swapper.wasm");
+                    const result = await WebAssembly.instantiateStreaming(
+                        bytes,
+                        go.importObject
+                    );
+                    mod = result.module;
+                    inst = result.instance;
+                    go.run(inst);
+                    setStatus("ready");
+                } catch (err) {
+                    console.error(err);
+                    setStatus("error");
+                }
+            }
 
-      goInit();
-    },
-    [] // Only re-run effect if script src changes
-  );
-  return status;
+            goInit();
+        },
+        [] // Only re-run effect if script src changes
+    );
+    return status;
 };
